6a40b0d205a309f18e025ae1915242736667cfb5
Ameena1BM23CS27 <ameenayasmeen.cs23@bmsce.ac.in>
2024-11-29 20:33:46 +0530

    Add files via upload

---
+++ java lab 10 (a)/IPCDemo.java
@@ -0,0 +1,57 @@
+class SharedResource {
+    private int data;
+    private boolean isDataAvailable = false;
+
+    
+    public synchronized void produceData(int data) throws InterruptedException {
+        while (isDataAvailable) {
+            wait(); 
+        }
+        this.data = data;
+        System.out.println("Produced: " + data);
+        isDataAvailable = true;
+        notify(); 
+    }
+
+   
+    public synchronized void consumeData() throws InterruptedException {
+        while (!isDataAvailable) {
+            wait(); 
+        }
+        System.out.println("Consumed: " + data);
+        isDataAvailable = false;
+        notify();  
+    }
+}
+
+public class IPCDemo {
+    public static void main(String[] args) {
+        SharedResource sharedResource = new SharedResource();
+
+        
+        Thread producer = new Thread(() -> {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    sharedResource.produceData(i);
+                    Thread.sleep(1000);  
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        
+        Thread consumer = new Thread(() -> {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    sharedResource.consumeData();
+                    Thread.sleep(1500);  
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        producer.start();
+        consumer.start();
+    }
