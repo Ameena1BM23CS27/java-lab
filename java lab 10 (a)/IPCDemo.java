9e08211d6dfcf76b84159ad82fd1909fd3d15fec
Ameena1BM23CS27 <ameenayasmeen.cs23@bmsce.ac.in>
2024-12-05 18:46:44 +0530

    Update IPCDemo.java

--- java lab 10 (a)/IPCDemo.java
+++ java lab 10 (a)/IPCDemo.java
@@ -2,50 +2,49 @@ class SharedResource {
     private int data;
     private boolean isDataAvailable = false;
 
-    
     public synchronized void produceData(int data) throws InterruptedException {
         while (isDataAvailable) {
-            wait(); 
+            wait();
         }
         this.data = data;
         System.out.println("Put: " + data);
         isDataAvailable = true;
-        notify(); 
+        notify();
     }
 
-   
     public synchronized void consumeData() throws InterruptedException {
         while (!isDataAvailable) {
-            wait(); 
+            wait();
         }
         System.out.println("Got: " + data);
         isDataAvailable = false;
-        notify();  
+        notify();
     }
 }
 
 public class IPCDemo {
     public static void main(String[] args) {
+        // Print your name once at the beginning
+        System.out.println("Ameena Yasmeen\n 1BM23CS027");
+
         SharedResource sharedResource = new SharedResource();
 
-        
         Thread producer = new Thread(() -> {
             try {
                 for (int i = 0; i < 5; i++) {
                     sharedResource.produceData(i);
-                    Thread.sleep(1000);  
+                    Thread.sleep(1000);
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         });
 
-        
         Thread consumer = new Thread(() -> {
             try {
                 for (int i = 0; i < 5; i++) {
                     sharedResource.consumeData();
-                    Thread.sleep(1500);  
+                    Thread.sleep(1500);
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -55,3 +54,4 @@ public class IPCDemo {
         producer.start();
         consumer.start();
     }
+}
